d4d4e35928b6ce635fe3cf1abe68c8ad1a097e2e
Kiran <kiran.bme@gmail.com>
2020-10-04 11:12:47 +0100

    firstcommit

---
+++ sample2.java
@@ -0,0 +1,22 @@
+
+public class sample2 {
+
+	public static void main(String[] args) {
+	
+int x=1;
+{
+ if(x<3)
+ {
+	 System.out.print("hi");
+	 
+ }
+ else 
+ {
+	 System.out.println("Bye");
+ } 
+ 
+
+}
+}
+}
+
